e52c174cc922e9d627c05bf00c0fc05fd4902943
ijesb <ijesb@STINKINDOWS>
2024-07-06 22:22:33 +0100

    Add project files.

---
+++ Console Games/Plugin.cs
@@ -0,0 +1,20 @@
+﻿﻿using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Events;
+
+namespace Console_Games
+{
+    public class Plugin
+    {
+
+        [PluginEntryPoint("Console Games", "1.0.0", "Console games to play while spectating in SCP SL", "SpiderBuh")]
+        public void OnPluginStart()
+        {
+
+            Log.Info($"Loading Console Games...");
+
+            EventManager.RegisterEvents<Events>(this);
+
+        }
+    }
+}
